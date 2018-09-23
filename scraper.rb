c832bf544b45ce118b6fc5c3b7d3832ea153f1b2
Tony Bowden <tony@tmtm.com>
2018-09-23 11:02:14 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,39 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'scraped'
+require 'scraperwiki'
+
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class MemberList < Scraped::JSON
+  field :members do
+    json.dig(:AllMembersList, :Member).map { |m| fragment(m => Member) }.uniq(&:id).map(&:to_h)
+  end
+end
+
+class Member < Scraped::JSON
+  field :id do
+    json[:PersonId]
+  end
+
+  field :name do
+    json[:MemberName]
+  end
+
+  field :family_name do
+    json[:MemberLastName]
+  end
+
+  field :given_name do
+    json[:MemberFirstName]
+  end
+
+  field :image do
+    json[:MemberImgUrl]
+  end
+end
+
+url = 'http://data.niassembly.gov.uk/members.asmx/GetAllMembers_JSON?'
+Scraped::Scraper.new(url => MemberList).store(:members)
